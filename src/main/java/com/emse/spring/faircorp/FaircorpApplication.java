35f8f18be0cd4f69dd822146636d44f773b032bb
jbjoannic <jb.joannic@etu.emse.fr>
2021-09-29 11:54:42 +0200

    29/09 fin

--- src/main/java/com/emse/spring/faircorp/FaircorpApplication.java
+++ src/main/java/com/emse/spring/faircorp/FaircorpApplication.java
@@ -3,6 +3,8 @@ package com.emse.spring.faircorp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class FaircorpApplication {
 
--- src/main/java/com/emse/spring/faircorp/hello/AnotherConsoleGreetingService.java
+++ src/main/java/com/emse/spring/faircorp/hello/AnotherConsoleGreetingService.java
@@ -1,13 +1,13 @@
 package com.emse.spring.faircorp.hello;
 
-
+import com.emse.spring.faircorp.hello.GreetingService;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-
+@Primary
 @Service
-public class ConsoleGreetingService implements GreetingService{
+public class AnotherConsoleGreetingService implements GreetingService {
     public void greet(String name){
-        System.out.println("Hello, "+name+"!");
+        System.out.println("Bonjour, "+name+"!");
     }
 }
