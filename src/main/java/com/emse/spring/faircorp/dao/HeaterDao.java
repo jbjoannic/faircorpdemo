23a04e391445db96cf91b0c432ca4173434da647
jbjoannic <jb.joannic@etu.emse.fr>
2021-10-19 14:53:40 +0200

    jb

--- src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
@@ -2,6 +2,14 @@ package com.emse.spring.faircorp.dao;
 import com.emse.spring.faircorp.model.Heater;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HeaterDao extends JpaRepository<Heater, Long> {
+
+
+    @Modifying
+    @Query("delete from Heater h where h.room.id = :id")
+    void deleteByRoom(@Param("id") Long id);
 }
--- src/test/java/com/emse/spring/faircorp/dao/HeaterDaoTest.java
+++ src/test/java/com/emse/spring/faircorp/dao/HeaterDaoTest.java
@@ -2,6 +2,7 @@ package com.emse.spring.faircorp.dao;
 
 import com.emse.spring.faircorp.model.Heater;
 import com.emse.spring.faircorp.model.Status;
+import com.emse.spring.faircorp.model.Window;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,13 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Set;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 class HeaterDaoTest {
 
     @Autowired
     private HeaterDao heaterDao;
-
+    @Autowired
+    private RoomDao roomDao;
     @Test
     public void shouldFindAHeater() {
         Heater heater = heaterDao.getById(-10L);
